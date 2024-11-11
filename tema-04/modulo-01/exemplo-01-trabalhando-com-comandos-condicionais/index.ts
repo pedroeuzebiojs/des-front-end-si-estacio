fb9b295b58fe0b4fac4604c41b8871f1423e49cd
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2024-11-11 17:35:51 -0300

    adiciona index.ts do exemplo 01 trabalhando com comandos condicionais do módulo 01 do tema 04

---
+++ tema-04/modulo-01/exemplo-01-trabalhando-com-comandos-condicionais/index.ts
@@ -0,0 +1,10 @@
+let mediaFinal: number = 8.5;
+if (mediaFinal == 10) {
+  console.log("Resultado excelente!");
+} else if (mediaFinal > 8) {
+  console.log("Parabéns!");
+} else if (mediaFinal >= 7) {
+  console.log("Aprovado!");
+} else {
+  console.log("vamos estudar mais!");
+}
