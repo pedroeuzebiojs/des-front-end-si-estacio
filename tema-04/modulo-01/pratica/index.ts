50d95a9708596b53a544c499d44711790ca15bd8
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2024-11-11 17:38:36 -0300

    adiciona index.ts da prática do módulo 01 do tema 04

---
+++ tema-04/modulo-01/pratica/index.ts
@@ -0,0 +1,17 @@
+/*
+troca = verdadeiro
+enquanto (troca)
+    início
+        troca = falso
+        para i = 0 ate N-1 faça
+              início
+                  se (vetor[i] > vetor[i+1])
+                        inicio
+                            troca = verdadeiro
+                            aux = vetor[i]
+                            vetor [i] = vetor[i+1]
+                            vetor [i] = aux
+                        fim
+                fim
+      fim
+*/
